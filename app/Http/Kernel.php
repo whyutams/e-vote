ba41a63d1ab07bf38945c8157ea52e429cefa819
ncrnt <wahyustamuu@gmail.com>
2025-08-27 14:55:56 +0800

    perbaikan routes & middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -40,7 +40,7 @@ class Kernel extends HttpKernel
 
         'api' => [
             // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
-            \Illuminate\Routing\Middleware\ThrottleRequests::class.':api',
+            \Illuminate\Routing\Middleware\ThrottleRequests::class . ':api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
     ];
@@ -63,8 +63,7 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        'superadmin' => \App\Http\Middleware\SuperAdminMiddleware::class,
-        'admin' => \App\Http\Middleware\AdminMiddleware::class,
-        'user' => \App\Http\Middleware\UserMiddleware::class,
+        'role' => \App\Http\Middleware\RoleMiddleware::class,
+
     ];
 }
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\ElectionController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\VoteController;
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -38,10 +39,11 @@ Route::middleware('auth')->group(function () {
     Route::post('/profile/update', [UserController::class, 'proses_update_profile'])->name('profile.update.submit');
     Route::post('/logout', [UserController::class, 'logout'])->name('logout');
 
+    // SUPERADMIN & ADMIN
     Route::prefix('dashboard')
-        ->name('dashboard.')->middleware(['superadmin', 'admin'])
+        ->name('dashboard.')->middleware(['role:' . User::ROLE_SUPERADMIN . ',' . User::ROLE_ADMIN])
         ->group(function () {
-            Route::get('', [DashboardController::class, "index"])->name("dashboard");
+            Route::get('', [DashboardController::class, "index"])->name("index");
 
             Route::controller(UserController::class)->group(function () {
                 Route::get('users', 'd')->name('users.index');
@@ -49,12 +51,23 @@ Route::middleware('auth')->group(function () {
                 Route::post('users', 'proses_register')->name('users.store');
                 Route::get('users/{user}/edit', 'd_edit')->name('users.edit');
                 Route::put('users/{user}', 'proses_update_profile')->name('users.update');
-                Route::delete('users/{user}', 'd_destroy')->name('users.destroy')->middleware('superadmin');
+                Route::delete('users/{user}', 'd_destroy')->name('users.destroy')->middleware('role:' . User::ROLE_SUPERADMIN);
             });
 
             Route::resource('elections', ElectionController::class)->names('elections');
             Route::resource('elections.candidates', CandidateController::class)->names('candidates');
-            Route::resource('elections.candidates.votes', VoteController::class)->names('votes')->only(['store', 'destroy']);
-
         });
+
+
+    // USER
+    Route::middleware('role:' . User::ROLE_USER)->group(function () {
+        Route::get('elections', [ElectionController::class, 'index'])->name('elections.index');
+        Route::get('elections/{election}', [ElectionController::class, 'show'])->name('elections.show');
+
+        Route::get('elections/{election}/candidates/{candidate}', [CandidateController::class, 'show'])->name('candidate.show');
+
+        // vote (create & delete)
+        Route::post('elections/{election}/candidates/{candidate}/vote', [VoteController::class, 'store'])->name('votes.store');
+        Route::delete('elections/{election}/candidates/{candidate}/vote', [VoteController::class, 'destroy'])->name('votes.destroy');
+    });
 });
