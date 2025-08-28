9eee2a986fbaa0a66022e4cb0ecb69cea3f7c9a6
ncrnt <wahyustamuu@gmail.com>
2025-08-28 21:27:15 +0800

    update tabel users

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,7 +17,6 @@ return new class extends Migration {
             $table->string('callname', 10)->nullable();
             $table->string('email', 100)->unique()->nullable();
             $table->string('phone', 20)->unique()->nullable();
-            $table->string('photo_profile')->nullable();
             $table->string('username', 50)->unique();
             $table->string('password');
             $table->enum('role', [User::ROLE_USER, User::ROLE_ADMIN, User::ROLE_SUPERADMIN])->default(User::ROLE_USER);
